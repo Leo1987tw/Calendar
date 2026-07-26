2482d5b23748ffa2c4bd5cd93bfbcf54797badbd
Leo <Leo1987tw@gmail.com>
2026-07-26 21:29:23 +0800

    add calendar.sql

--- calendar.sql
+++ calendar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2026-07-19 14:02:45
+-- 產生時間： 2026-07-26 21:28:59
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -42,17 +42,6 @@ CREATE TABLE `events` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- 傾印資料表的資料 `events`
---
-
-INSERT INTO `events` (`id`, `event_date`, `start_time`, `end_time`, `title`, `description`, `bg_color`, `border_color`, `created_at`, `deleted_at`) VALUES
-(1, '2026-07-14', '00:00:00', '23:59:59', 'test', '', '#00FFFF', '', '2026-07-12 05:20:28', NULL),
-(2, '2026-07-15', '00:05:57', '07:05:57', 'asdf', '', '#115678', '', '2026-07-19 01:06:39', NULL),
-(3, '2026-07-15', '08:05:57', '09:05:57', 'asdf', '', '#115678', '', '2026-07-19 01:06:42', NULL),
-(4, '2026-07-15', '11:07:17', '14:11:17', 'fasdf', '', '#154865', '', '2026-07-19 01:07:46', NULL),
-(5, '2026-07-15', '15:07:17', '23:59:59', 'fasdf', '', '#154865', '', '2026-07-19 01:07:49', NULL);
-
---
 -- 已傾印資料表的索引
 --
 
@@ -70,7 +59,7 @@ ALTER TABLE `events`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
